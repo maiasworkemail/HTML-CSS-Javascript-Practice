fa06a7f9ae7748ab83dbae7b011bf50f92cd0f4b
maiasworkemail <45532897+maiasworkemail@users.noreply.github.com>
2018-12-25 13:46:14 -0600

    Object Variable with Properties
    I added a variable of type object which has 3 object properties (I added a comment describing it as well).

--- Javascript-Practice.js
+++ Javascript-Practice.js
@@ -14,4 +14,8 @@ var integerVar=2018;  //variable of data type "integer"
 var stringVar="text"; //variable of data type "string"  
 var booleanVar= true; //variable of data type "boolean" 
 
-var varNullObject=null; //variable of data type object (null is of type object, not null)
+var varNullObject=null; 
+//variable of data type object (null is of type object, not null)
+//you can empty an object by setting it to null or undefined
+var varListObject={objectName1:"object1Value1", objectName2:"object1Value2", objectName3:"object1Value3"};
+//above is an object with three properties: objectName1, objectName2, and objectName3
