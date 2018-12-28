92dabd99adca67ab0923484c0b396869999ca6cc
maiasworkemail <45532897+maiasworkemail@users.noreply.github.com>
2018-12-27 21:41:12 -0600

    Spaces before and after equal sign
    I made sure to add one space before and one space after each equal sign if there wasn't one already for each variable assignment.

--- Javascript-Practice.js
+++ Javascript-Practice.js
@@ -5,29 +5,29 @@
 var myAge; 
 //variable declaration 
 
-myAge=24; 
+myAge = 24; 
 //variable initialization 
 
 
 var undefinedVar;     //variable of data type "undefined" (since there's no value initialized to it)
                       //if this gets added with an integer, an error message "NaN" appears (aka "Not a Number")
-var integerVar=2018;  //variable of data type "integer"
+var integerVar = 2018;  //variable of data type "integer"
 var stringVar="text"; //variable of data type "string"  
 var booleanVar= true; //variable of data type "boolean" 
 
-var varNullObject=null; 
+var varNullObject = null; 
 //variable of data type object (null is of type object, not null)
 //you can empty an object by setting it to null or undefined
-var varListObject={objectName1:"object1Value1", objectName2:"object1Value2", objectName3:"object1Value3"};
+var varListObject = {objectName1:"object1Value1", objectName2:"object1Value2", objectName3:"object1Value3"};
 //above is an object with three properties: objectName1, objectName2, and objectName3
 
-var varArray= ["arrayItem1", "arrayItem2", "arrayItem3"];
+var varArray = ["arrayItem1", "arrayItem2", "arrayItem3"];
 //this is an array with three array items
 
 
 
 
-var int1= 1; 
+var int1 = 1; 
 int1++; 
 console.log(int1); 
 
